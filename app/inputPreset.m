0f61accb2f6a27051277c8183adf6a9cdb0fcdfd
ezra <ezra@soniccloud.com>
2020-06-24 10:04:26 -0700

    add input presets

---
+++ app/inputPreset.m
@@ -0,0 +1,79 @@
+function inputs = inputPreset(idx)
+inputs = AppInputs();
+switch(idx)
+    case 1
+        
+        inputs.icATExpHR = 0;
+        inputs.icATRecHR = .01;
+        inputs.icATExpLR = .002;
+        inputs.icATRecLR = .05;
+        inputs.icStaffExpHR = 0;
+        inputs.icStaffRecHR = .01;
+        inputs.icStaffExpLR = .002;
+        inputs.icStaffRecLR = .05;
+        inputs.icSExpC = .015;
+        inputs.icSRecC = .08;
+        inputs.icSExpNC = .015;
+        inputs.icSRecNC = .08;
+        inputs.soATHR = .25;
+        inputs.soATLR = 0;
+        inputs.soStaffHR = 1/7;
+        inputs.soStaffLR = 0;
+        inputs.soSC = 1/7;
+        inputs.soSNC = 1/7;
+        inputs.propNC = 3/7;
+        inputs.scaleTracking = .8 * 100;
+        inputs.incAsymp = .75;
+        
+    case 2
+        
+        
+        inputs.icATExpHR = 0;
+        inputs.icATRecHR = .01;
+        inputs.icATExpLR = .002;
+        inputs.icATRecLR = .05;
+        inputs.icStaffExpHR = 0;
+        inputs.icStaffRecHR = .01;
+        inputs.icStaffExpLR = .002;
+        inputs.icStaffRecLR = .05;
+        inputs.icSExpC = .015;
+        inputs.icSRecC = .08;
+        inputs.icSExpNC = .015;
+        inputs.icSRecNC = .08;
+        inputs.soATHR = .25;
+        inputs.soATLR = 0;
+        inputs.soStaffHR = 1/7;
+        inputs.soStaffLR = 0;
+        inputs.soSC = 1/7;
+        inputs.soSNC = 1/7;
+        inputs.propNC = 3/7;
+        inputs.scaleTracking = .8 * 100;
+        inputs.incAsymp = .75;
+        
+    case 3
+        
+        
+        inputs.icATExpHR = 0;
+        inputs.icATRecHR = .01;
+        inputs.icATExpLR = .002;
+        inputs.icATRecLR = .05;
+        inputs.icStaffExpHR = 0;
+        inputs.icStaffRecHR = .01;
+        inputs.icStaffExpLR = .002;
+        inputs.icStaffRecLR = .05;
+        inputs.icSExpC = .015;
+        inputs.icSRecC = .08;
+        inputs.icSExpNC = .015;
+        inputs.icSRecNC = .08;
+        inputs.soATHR = .25;
+        inputs.soATLR = 0;
+        inputs.soStaffHR = 1/7;
+        inputs.soStaffLR = 0;
+        inputs.soSC = 1/7;
+        inputs.soSNC = 1/7;
+        inputs.propNC = 3/7;
+        inputs.scaleTracking = .8 * 100;
+        inputs.incAsymp = .75;
+        
+end
+end
